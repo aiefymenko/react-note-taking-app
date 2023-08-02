b45c775237084f2ac4ea5af1f4dc7d1fdfda4cae
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-08-01 20:35:52 -0400

    Create multi selectable form

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,5 @@
 import { Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from 'react-select/creatable'
 
 export function NoteForm() {
   return <Form>
@@ -13,7 +14,7 @@ export function NoteForm() {
         <Col>
         <Form.Group controlId="tags">
           <Form.Label>Tags</Form.Label>
-          <Form.Control required />
+          <CreatableReactSelect isMulti />
         </Form.Group>
         </Col>
       </Row>
