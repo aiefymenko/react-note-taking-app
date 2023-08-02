cbc028d76490cff69754529687b080ad2a0f31fa
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-08-01 21:00:52 -0400

    Create types function to handle on submit

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,15 +1,29 @@
+import { FormEvent, useRef } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from 'react-select/creatable'
+import { NoteData } from "./App";
 
-export function NoteForm() {
-  return <Form>
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+export function NoteForm({onSubmit}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit (e: FormEvent) {
+    e.preventDefault();
+
+  }
+
+  return <Form onSubmit={handleSubmit}>
     <Stack gap={4}>
       <Row>
         <Col>
         <Form.Group controlId="title">
           <Form.Label>Title</Form.Label>
-          <Form.Control required />
+          <Form.Control ref={titleRef} required />
         </Form.Group>
         </Col>
         <Col>
@@ -21,7 +35,7 @@ export function NoteForm() {
       </Row>
       <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as='textarea' rows={15} />
+          <Form.Control ref={markdownRef} required as='textarea' rows={15} />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">Submit</Button>
