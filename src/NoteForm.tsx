6e6126d4653fda59799095eb029634fc983c5905
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-08-01 20:41:56 -0400

    Create Submit and Cancel buttons

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,4 @@
-import { Col, Form, Row, Stack } from "react-bootstrap";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import CreatableReactSelect from 'react-select/creatable'
 
 export function NoteForm() {
@@ -18,6 +18,14 @@ export function NoteForm() {
         </Form.Group>
         </Col>
       </Row>
+      <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as='textarea' rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">Submit</Button>
+          <Button type="button" variant="outline-secondary">Cancel</Button>
+        </Stack>
     </Stack>
   </Form>
 }
