652e525c8f93c7112921348af3829bda7533c1a9
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-08-12 14:34:08 -0400

    Pass the property of availabletags

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,16 @@
-import { Button, Col, Row, Stack } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Tag } from "./App";
 
-export function NoteList() {
+type NoteListProps = {
+  availableTags: Tag[];
+}
+
+export function NoteList({availableTags}: NoteListProps ) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+  
   return <>
   <Row>
     <Col><h1>Notes</h1></Col>
@@ -14,5 +23,31 @@ export function NoteList() {
       </Stack>
     </Col>
   </Row>
+  <Form>
+    <Row className="mb-4">
+      <Col>
+      <Form.Group controlId="title">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text"></Form.Control>
+        </Form.Group></Col>
+        <Col>
+        <Form.Group controlId="tags">
+          <Form.Label>Tags</Form.Label>
+          <ReactSelect
+           value={selectedTags.map(tag => {
+            return {label: tag.label, value: tag.id}
+          })} 
+          options={availableTags.map(tag => {
+            return {label: tag.label, value: tag.id}
+          })}
+          onChange={tags => {
+            setSelectedTags(tags.map(tag => {
+              return {label: tag.label, id: tag.value}
+            }))
+          }} isMulti />
+        </Form.Group>
+        </Col>
+    </Row>
+  </Form>
   </>
 }
