76f8be3ec55194543e15d6707da866b8c95e09be
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-08-26 21:00:01 -0400

    CHange onSubmit with appropriate properties

--- src/EditNote.tsx
+++ src/EditNote.tsx
@@ -1,5 +1,6 @@
 import { NoteData, Tag } from "./App"
 import { NoteForm } from "./NoteForm"
+import { useNote } from "./NoteLayout"
 
 type EditNoteProps = {
   onSubmit: (id: string, data: NoteData) => void
@@ -8,8 +9,9 @@ type EditNoteProps = {
 }
 
 export function EditNote({onSubmit, onAddTag, availableTags}: EditNoteProps) {
+  const note = useNote()
   return <>
   <h1 className="mb-4"> Edit Note </h1>
-  <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
+  <NoteForm onSubmit={data => onSubmit(note.id, data)} onAddTag={onAddTag} availableTags={availableTags} />
   </>
 }
